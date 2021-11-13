4aa9589a8a80415ead278394c950abbdbe2569ba
Maxime Eloir <maxime.eloir@gmail.com>
2021-11-13 16:00:11 +0100

    Correction de l'overflow du menu

--- targetWebKitchen/js/main.js
+++ targetWebKitchen/js/main.js
@@ -15,6 +15,7 @@
 
    var ham = $("button.hamburger");
    var menu = $(".mobile")
+   var body = $("body")
    console.log(ham)
    console.log(menu)
 
@@ -23,9 +24,12 @@
       if (menu.hasClass("showMenu")) {
          menu.removeClass("showMenu");
          ham.removeClass("is-active")
+         body.removeClass("overflow-hidden");
+
       } else {
          menu.addClass("showMenu");
          ham.addClass("is-active hamburger--spring");
+         body.addClass("overflow-hidden")
       }
    }
    ham.click(toggleMenu)
